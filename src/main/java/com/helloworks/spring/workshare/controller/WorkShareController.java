835c6418b415294aea3773c3fd3822aa6310d445
DH_furaha <80407403+pastelto@users.noreply.github.com>
2021-10-18 12:44:09 +0900

    211018 [다혜] 성현님 풀 머지 테스트 - 커밋

--- src/main/java/com/helloworks/spring/workshare/controller/WorkShareController.java
+++ src/main/java/com/helloworks/spring/workshare/controller/WorkShareController.java
@@ -14,36 +14,46 @@ public class WorkShareController {
 	
 	// 메뉴바 -> 미확인 업무
 	@RequestMapping("unCheckedListWS.ws")
-	public String unCheckedWorkShare() {
+	public String unCheckedList() {
 		System.out.println("미확인 업무 메인화면으로 이동");
 		return "workShare/workShareAllPage";
 	}
 	
 	// 메뉴바 -> 수신 업무 내역
 	@RequestMapping("recvListWS.ws")
-	public String checkedWorkShare() {
+	public String checkedList() {
 		System.out.println("수신내역 메인화면으로 이동");
 		return "workShare/workShareAllPage";
 	}
 	
 	// 메뉴바 -> 발신 업무 내역
 	@RequestMapping("sendListWS.ws")
-	public String sendWorkShare() {
+	public String sendList() {
 		System.out.println("발신 업무 메인화면으로 이동");
 		return "workShare/workShareAllPage";
 	}
 	
 	// 메뉴바 -> 임시저장 업무 내역
 	@RequestMapping("savedListWS.ws")
-	public String savedWorkShare() {
+	public String savedList() {
 		System.out.println("임시저장 업무 메인화면으로 이동");
 		return "workShare/workShareAllPage";
 	}
 	
-	// 업무공유 작성
+	// 업무공유 작성 페이지 이동
 	@RequestMapping("sendFormView.ws")
 	public String sendFormView() {
 		System.out.println("임시저장 업무 메인화면으로 이동");
 		return "workShare/sendWSForm";
+	}
+	
+	// 업무공유 작성 저장
+	@RequestMapping("sendWS.ws")
+	public String sendWorkShare() {
+		System.out.println("업무공유 작성 저장중");
+		
+
+		
+		return "workShare/workShareAllPage";
 	}
 }
