c9c70bb3863b0e48ab48acc4e82813d161c88309
dayeo <dayeong815@gmail.com>
2021-10-20 18:15:54 +0900

    10-20[왕다영] 회의실등록 디비인서트 확인

--- src/main/java/com/helloworks/spring/request/model/dao/RequestDao.java
+++ src/main/java/com/helloworks/spring/request/model/dao/RequestDao.java
@@ -1,5 +1,16 @@
 package com.helloworks.spring.request.model.dao;
 
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.helloworks.spring.request.model.vo.Mtr;
+
+@Repository
 public class RequestDao {
+
+	public int addMtr(SqlSessionTemplate sqlSession, Mtr mtr) {
+		// TODO Auto-generated method stub
+		return sqlSession.insert("requestMapper.addMtr", mtr);
+	}
 
 }
