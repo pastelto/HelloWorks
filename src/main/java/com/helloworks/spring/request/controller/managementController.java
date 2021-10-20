232b4399fe06bc548a87328412db4b29f2a551e3
dayeo <dayeong815@gmail.com>
2021-10-20 10:28:53 +0900

    10-20[왕다영] 풀받기위한 커밋/ 메일페이지 수정, 회의실등록 기능 수정중

--- src/main/java/com/helloworks/spring/request/controller/managementController.java
+++ src/main/java/com/helloworks/spring/request/controller/managementController.java
@@ -1,7 +1,13 @@
 package com.helloworks.spring.request.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.helloworks.spring.request.model.vo.Mtr;
+
 
 @Controller
 public class managementController {
@@ -12,6 +18,18 @@ public class managementController {
 		System.out.println("회의실 관리페이지");
 		return "request/manageMeetingRoom";
 	}
+	
+	//회의실 등록 페이지
+		@RequestMapping("add.mtr")
+		public String addMtr(Mtr mtr, HttpServletRequest request, Model model) {
+			System.out.println("회의실 등록페이지");
+			System.out.println(mtr);
+			
+			
+			
+			return "request/addMeetingRoom";
+		}
+	
 	
 	//차량 관리 페이지로 전환
 	@RequestMapping("manage.car")
