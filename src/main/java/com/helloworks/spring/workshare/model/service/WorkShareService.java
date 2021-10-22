1ea3b755de3e8e41237589b37ec4254c68ca8e7b
DH_furaha <80407403+pastelto@users.noreply.github.com>
2021-10-22 20:30:27 +0900

    Revert "Revert "211022 [다혜] 첨부파일 조회 오류""
    This reverts commit dc7afc0bf8de007898549078512feeba51474da8.

--- src/main/java/com/helloworks/spring/workshare/model/service/WorkShareService.java
+++ src/main/java/com/helloworks/spring/workshare/model/service/WorkShareService.java
@@ -29,4 +29,8 @@ public interface WorkShareService {
 
 	ArrayList<WorkShare> selectSavedList(Employee myEmp, PageInfo pi) throws Exception;
 
+	WorkShare detailWS(int wno) throws Exception;
+
+	ArrayList<WSAttachment> detailWSAttachment(int wsno) throws Exception;
+
 }
