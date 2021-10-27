9a8c21b2704e26b50a280005acf87e742f199a4f
dayeo <dayeong815@gmail.com>
2021-10-27 11:50:38 +0900

    10-27[왕다영] 최신화하기위한 커밋, 신청내역수정중

--- src/main/java/com/helloworks/spring/request/controller/RequestController.java
+++ src/main/java/com/helloworks/spring/request/controller/RequestController.java
@@ -11,8 +11,6 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -23,7 +21,6 @@ import org.springframework.web.servlet.ModelAndView;
 import com.google.gson.GsonBuilder;
 import com.helloworks.spring.common.exception.CommException;
 import com.helloworks.spring.request.model.service.RequestService;
-import com.helloworks.spring.request.model.vo.Mtr;
 import com.helloworks.spring.request.model.vo.RequestEq;
 import com.helloworks.spring.request.model.vo.RequestId;
 
